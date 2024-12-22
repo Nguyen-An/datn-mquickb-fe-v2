9b4657346867c5e36999a9d97cef36c4e1c3c2c9
An Nguyen <vanan6b@gmail.com>
2024-12-23 00:51:12 +0700

    update code

--- src/api/file.ts
+++ src/api/file.ts
@@ -4,3 +4,13 @@ export async function uploadIamge(params?: any) {
     const getData = await sendPost(`uploadfile/upload/avatar`, params)
     return getData
 }
+
+export async function getDataFiles(params?: any) {
+    const data = await sendGet(`uploadfile/files`, params)
+    return data
+}
+
+export async function uploadFileToS3(params?: any) {
+    const getData = await sendPost(`uploadfile/upload/file-s3`, params)
+    return getData
+}
